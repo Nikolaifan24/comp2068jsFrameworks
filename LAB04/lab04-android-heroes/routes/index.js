fdddb0ab0fe35790293868eca93b10e8b0e5815b
Nikolai Fandino <nikolaifan@MacBook-Pro-de-Nikolai.local>
2025-11-30 23:18:00 -0500

    resolving errors

--- LAB04/lab04-android-heroes/routes/index.js
+++ LAB04/lab04-android-heroes/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('kotlin-team', { title: 'Android Heroes' });
+});
+
+router.get('/kotlin-team', (req, res) => {
+  res.render('kotlin-team', { title: 'Kotlin Team' });
+});
+
+router.get('/jetpack', (req, res) => {
+  res.render('jetpack', { title: 'Jetpack Compose' });
+});
+
+router.get('/firebase', (req, res) => {
+  res.render('firebase', { title: 'Firebase Team' });
+});
+
+router.get('/android-studio', (req, res) => {
+  res.render('android-studio', { title: 'Android Studio Team' });
 });
 
 module.exports = router;
